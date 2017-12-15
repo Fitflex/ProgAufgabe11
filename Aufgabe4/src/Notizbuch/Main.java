e08aeb0f9670e6fd61fdb51de075adba215e2501
Fitflex <marlonmail94@t-online.de>
2017-12-15 01:05:05 +0100

    Add files via upload

---
+++ Aufgabe4/src/Notizbuch/Main.java
@@ -0,0 +1,16 @@
+package Notizbuch;
+
+/**
+ * Ein Programm zum erstellen von Notizen die sich untereinander lesen.
+ * 
+ * @author Marlon, u29739, m21899
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Startfenster startfenster = new Startfenster();
+		startfenster.setVisible(true);
+	}
+
+}
